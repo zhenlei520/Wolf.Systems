0d03d8683ab19e7ee4760dbed7322cd3fbaafdf4
磊磊 <wangzhenlei520@gmail.com>
2021-02-23 19:02:20 +0800

    新增加查询继承type的接口与实现类集合

--- src/Wolf.Systems.Core/Common/TypeCommon.cs
+++ src/Wolf.Systems.Core/Common/TypeCommon.cs
@@ -2,9 +2,15 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Wolf.Systems.Core.Common
 {
+    /// <summary>
+    ///
+    /// </summary>
     public class TypeCommon
     {
         #region 获取类型
@@ -25,6 +31,40 @@ namespace Wolf.Systems.Core.Common
         public static Type GetType(Type type)
         {
             return Nullable.GetUnderlyingType(type) ?? type;
+        }
+
+        #endregion
+
+        #region 查询继承type的接口与实现类集合
+
+        /// <summary>
+        /// 查询继承type的接口与实现类集合
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static List<KeyValuePair<Type, Type>> GetInterfaceAndImplementationType(Assembly[] assemblies, Type type)
+        {
+            var fulleName = type.Namespace + type.Name;
+            var list = assemblies
+                .SelectMany(x =>
+                    x.GetTypes().Where(y => y.GetInterfaces().Any(z => (z.Namespace + z.Name).Equals(fulleName))))
+                .ToList();
+
+            var typeList = new List<KeyValuePair<Type, Type>>();
+            foreach (var interfaceType in list.Where(x => x.IsInterface))
+            {
+                var interfaceTypeFullName = interfaceType.Namespace + interfaceType.Name;
+
+                list.Where(x =>
+                    !x.IsInterface && x.GetInterfaces().Any(z =>
+                        (z.Namespace + z.Name).Equals(interfaceTypeFullName))).ToList().ForEach(implementationType =>
+                {
+                    typeList.Add(new KeyValuePair<Type, Type>(interfaceType, implementationType));
+                });
+            }
+
+            return typeList;
         }
 
         #endregion
