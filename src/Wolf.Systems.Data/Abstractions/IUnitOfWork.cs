9e2cb3aa480209542b8b212351c68a3c66aa380b
zhenlei520 <wangzhenlei520@gmail.com>
2021-07-05 00:47:12 +0800

    更改IUnitwork

--- src/Wolf.Systems.Data/Abstractions/IUnitOfWork.cs
+++ src/Wolf.Systems.Data/Abstractions/IUnitOfWork.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) zhenlei520 All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace Wolf.Systems.Data.Abstractions
 {
     /// <summary>
@@ -11,24 +8,6 @@ namespace Wolf.Systems.Data.Abstractions
     /// </summary>
     public interface IUnitOfWork<TDbContext> : IDbContext
     {
-        /// <summary>
-        /// 提交保存
-        /// </summary>
-        /// <returns></returns>
-        bool Commit();
 
-        /// <summary>
-        /// 异步保存
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
-
-        /// <summary>
-        /// 异步保存
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
